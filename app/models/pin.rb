02d5bfb2c86e88cd8bb95b05fd24b09ffedee0e8
Nico Miceli <nicomiceli@gmail.com>
2013-11-23 23:09:21 -0500

    fix forms

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -9,9 +9,8 @@ class Pin < ActiveRecord::Base
                             size: { less_than: 5.megabytes }
   belongs_to :user
 
-	def image_remote_url=(url_value)
+	def image_remote_url = (url_value)
 		super
 		self.image = URI.parse(url_value) unless url_value.blank?
-		
-	end
+		end
 end
